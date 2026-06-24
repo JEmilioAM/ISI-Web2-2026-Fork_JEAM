190f7647f034380022d6236a0de24e48a480c8bf
JEmilioAM <jeffry.alvarado24902814@estu.unan.edu.ni>
2026-06-23 19:07:07 -0600

    login de EMILIO

---
+++ pages/admin/EMILIO/login/login.js
@@ -0,0 +1,39 @@
+const baseUrl = "https://localhost:7286/api";
+
+async function handleLoginSubmit(event) {
+    event.preventDefault();
+
+    const formData = new FormData(event.target);
+    const login = Object.fromEntries(formData.entries());
+    
+    //Para evidenciar el usuario y password ingresado
+    console.log('Login data:', login);
+
+    const endpoint = `${baseUrl}/authentication/login`;
+    const response = await fetch(endpoint, {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify(login)
+
+    });
+
+    const data = await response.json();
+    
+    if (response.ok) {
+        const tokenString = JSON.stringify(data);
+        
+        localStorage.setItem('token', tokenString);
+
+        //Para evidenciar token utilizado que permite acceder a los endpoints de mi API
+        console.log('Login succesful:', data.token);
+        
+        setTimeout(() => {
+            window.location.href = window.location.origin + '/pages/admin/EMILIO/cards/cards.html';
+        }, 5000);
+
+    } else {
+        console.log('Login failed: ' + data.message);
+
+    }
+
+}
